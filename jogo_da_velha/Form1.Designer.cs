71df2f5f485fddd8c11498010577f59621d75777
Aluno <Aluno@SNCCHLAB03F315>
2022-11-19 11:06:52 -0300

    Adicionar arquivos de projeto.

---
+++ jogo_da_velha/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace jogo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cell1 = new System.Windows.Forms.Button();
+            this.cell2 = new System.Windows.Forms.Button();
+            this.cell3 = new System.Windows.Forms.Button();
+            this.cell4 = new System.Windows.Forms.Button();
+            this.cell5 = new System.Windows.Forms.Button();
+            this.cell6 = new System.Windows.Forms.Button();
+            this.cell7 = new System.Windows.Forms.Button();
+            this.cell8 = new System.Windows.Forms.Button();
+            this.cell9 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.Label();
+            this.txtO = new System.Windows.Forms.Label();
+            this.txtTurn = new System.Windows.Forms.Label();
+            this.btnresetar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cell1
+            // 
+            this.cell1.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell1.Location = new System.Drawing.Point(96, 42);
+            this.cell1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell1.Name = "cell1";
+            this.cell1.Size = new System.Drawing.Size(80, 80);
+            this.cell1.TabIndex = 0;
+            this.cell1.UseVisualStyleBackColor = false;
+            // 
+            // cell2
+            // 
+            this.cell2.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell2.Location = new System.Drawing.Point(182, 42);
+            this.cell2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell2.Name = "cell2";
+            this.cell2.Size = new System.Drawing.Size(80, 80);
+            this.cell2.TabIndex = 1;
+            this.cell2.UseVisualStyleBackColor = false;
+            // 
+            // cell3
+            // 
+            this.cell3.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell3.Location = new System.Drawing.Point(268, 42);
+            this.cell3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell3.Name = "cell3";
+            this.cell3.Size = new System.Drawing.Size(80, 80);
+            this.cell3.TabIndex = 2;
+            this.cell3.UseVisualStyleBackColor = false;
+            // 
+            // cell4
+            // 
+            this.cell4.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell4.Location = new System.Drawing.Point(96, 128);
+            this.cell4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell4.Name = "cell4";
+            this.cell4.Size = new System.Drawing.Size(80, 80);
+            this.cell4.TabIndex = 3;
+            this.cell4.UseVisualStyleBackColor = false;
+            // 
+            // cell5
+            // 
+            this.cell5.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell5.Location = new System.Drawing.Point(182, 128);
+            this.cell5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell5.Name = "cell5";
+            this.cell5.Size = new System.Drawing.Size(80, 80);
+            this.cell5.TabIndex = 4;
+            this.cell5.UseVisualStyleBackColor = false;
+            // 
+            // cell6
+            // 
+            this.cell6.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell6.Location = new System.Drawing.Point(268, 128);
+            this.cell6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell6.Name = "cell6";
+            this.cell6.Size = new System.Drawing.Size(80, 80);
+            this.cell6.TabIndex = 5;
+            this.cell6.UseVisualStyleBackColor = false;
+            // 
+            // cell7
+            // 
+            this.cell7.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell7.Location = new System.Drawing.Point(96, 214);
+            this.cell7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell7.Name = "cell7";
+            this.cell7.Size = new System.Drawing.Size(80, 80);
+            this.cell7.TabIndex = 6;
+            this.cell7.UseVisualStyleBackColor = false;
+            // 
+            // cell8
+            // 
+            this.cell8.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell8.Location = new System.Drawing.Point(182, 214);
+            this.cell8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell8.Name = "cell8";
+            this.cell8.Size = new System.Drawing.Size(80, 80);
+            this.cell8.TabIndex = 7;
+            this.cell8.UseVisualStyleBackColor = false;
+            // 
+            // cell9
+            // 
+            this.cell9.BackColor = System.Drawing.Color.Gainsboro;
+            this.cell9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cell9.Location = new System.Drawing.Point(268, 214);
+            this.cell9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cell9.Name = "cell9";
+            this.cell9.Size = new System.Drawing.Size(80, 80);
+            this.cell9.TabIndex = 8;
+            this.cell9.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtO);
+            this.groupBox1.Controls.Add(this.txtX);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(96, 385);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Placar:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jogador X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jogador O";
+            // 
+            // txtX
+            // 
+            this.txtX.AutoSize = true;
+            this.txtX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtX.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtX.Location = new System.Drawing.Point(109, 26);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(18, 20);
+            this.txtX.TabIndex = 2;
+            this.txtX.Text = "0";
+            // 
+            // txtO
+            // 
+            this.txtO.AutoSize = true;
+            this.txtO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtO.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtO.Location = new System.Drawing.Point(109, 61);
+            this.txtO.Name = "txtO";
+            this.txtO.Size = new System.Drawing.Size(18, 20);
+            this.txtO.TabIndex = 3;
+            this.txtO.Text = "0";
+            // 
+            // txtTurn
+            // 
+            this.txtTurn.AutoSize = true;
+            this.txtTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTurn.ForeColor = System.Drawing.Color.Blue;
+            this.txtTurn.Location = new System.Drawing.Point(350, 402);
+            this.txtTurn.Name = "txtTurn";
+            this.txtTurn.Size = new System.Drawing.Size(65, 16);
+            this.txtTurn.TabIndex = 10;
+            this.txtTurn.Text = "O jogou!";
+            // 
+            // btnresetar
+            // 
+            this.btnresetar.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnresetar.Location = new System.Drawing.Point(336, 421);
+            this.btnresetar.Name = "btnresetar";
+            this.btnresetar.Size = new System.Drawing.Size(92, 45);
+            this.btnresetar.TabIndex = 11;
+            this.btnresetar.Text = "Resetar";
+            this.btnresetar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(464, 501);
+            this.Controls.Add(this.btnresetar);
+            this.Controls.Add(this.txtTurn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cell9);
+            this.Controls.Add(this.cell8);
+            this.Controls.Add(this.cell7);
+            this.Controls.Add(this.cell6);
+            this.Controls.Add(this.cell5);
+            this.Controls.Add(this.cell4);
+            this.Controls.Add(this.cell3);
+            this.Controls.Add(this.cell2);
+            this.Controls.Add(this.cell1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cell1;
+        private System.Windows.Forms.Button cell2;
+        private System.Windows.Forms.Button cell3;
+        private System.Windows.Forms.Button cell4;
+        private System.Windows.Forms.Button cell5;
+        private System.Windows.Forms.Button cell6;
+        private System.Windows.Forms.Button cell7;
+        private System.Windows.Forms.Button cell8;
+        private System.Windows.Forms.Button cell9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtO;
+        private System.Windows.Forms.Label txtX;
+        private System.Windows.Forms.Label txtTurn;
+        private System.Windows.Forms.Button btnresetar;
+    }
+}
+
---
+++ jogo_da_velha/Ojogo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo_da_velha
+{
+    class Ojogo
+    {
+        public Ojogo()
+        {
+            initBoard();
+        }
+
+        private void initBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
